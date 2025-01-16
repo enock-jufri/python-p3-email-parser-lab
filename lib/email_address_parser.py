4e5c844ddf0ffd6744f7726ef842753441da7faf
enock-jufri <enockjufri@gmail.com>
2025-01-16 16:04:30 +0300

    email parser

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self,text):
+        self.text=text
+    
+    def parse(self):
+        emails= re.findall(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+',self.text)
+        return sorted(set(emails))
+    
+parser = EmailAddressParser("talk@talk.com, what john.jones@flatironschool.com, who alexa@amazon.com, where when why")
+print(parser.parse())
